a275f660c87f42620408f6508bb3aa677b08fc11
Xmoncoco <hadjadji.milhan@icloud.com>
2025-11-15 10:17:38 +0100

    currently adding an update logic

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+SCRIPT_DIR = $(dirname " &(readlink -f "$0")") 
+REP_ORIGINE = $(pwd)
+echo("🆙 : NEW VERSION FOUND UPDATING")
+mkdir $SCRIPT_DIR/../tempqddlkmfjqfejie
+cp $SCRIPT_DIR/config.toml $SCRIPT_DIR/../tempqddlkmfjqfejie/config.toml
+cp $SCRIPT_DIR/history_of_download.sqlite $SCRIPT_DIR/../tempqddlkmfjqfejie/history_of_download.sqlite
+cp $SCRIPT_DIR/.env $SCRIPT_DIR/../tempqddlkmfjqfejie/.env
+git clone  https://github.com/Xmoncoco/palemachine.git $SCRIPT_DIR/../palemachine
+rm -rf $SCRIPT_DIR/*
+cd ..
+./palemachine/installation_scripts/build.sh
+rm package/.env
+cp -R ./tempqddlkmfjqfejie/* /package/
+rm -R ./tempqddlkmfjqfejie
