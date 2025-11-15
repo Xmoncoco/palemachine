f1eb2d27295ed3a9a1b465a12a1ca285b7194d1a
Xmoncoco <hadjadji.milhan@icloud.com>
2025-11-15 20:30:29 +0100

    just added .version to the compiler

--- installation_scripts/build.sh
+++ installation_scripts/build.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 mkdir package
 cargo build --release --manifest-path $SCRIPT_DIR/../Cargo.toml --target-dir ./package
 cd package
-cp -r $SCRIPT_DIR/../{bambam_morigatsu_chuapo.sh,config.toml,pages,downloader,requirement.txt,update.sh} ./
+cp -r $SCRIPT_DIR/../{.version,bambam_morigatsu_chuapo.sh,config.toml,pages,downloader,requirement.txt,update.sh} ./
 python3 -m venv venv 
 source ./venv/bin/activate
 ./venv/bin/pip install -r requirement.txt
